22bd2cb74f9a7f0e0d42c178dc2fe8fc9f8c1e61
Łukasz Walaszczyk <l.walaszczyk@interia.pl>
2020-10-12 22:56:12 +0200

    refactor 'useForm' hook

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,37 +4,37 @@ import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 import useForm from "../hooks/useForm.js";
 
 const Form = ({ setTodos }) => {
-  const stateSchema = {
-    taskName: { value: "", error: "" },
-    taskPriority: { value: "", error: "" },
-  };
-
-  const stateValidatorSchema = {
-    taskName: {
-      required: true,
-      validator: {
-        func: (value) => value.length <= 40,
-        error: "Task name too long",
-      },
-    },
-    taskPriority: {
-      required: false,
-    },
-  };
-
   function onSubmitForm(todo) {
     const newTodo = { ...todo, taskDone: false };
     setTodos((prevTodos) => [...prevTodos, newTodo]);
   }
 
+  const formStateSchema = {
+    taskName: {
+      getErrorMsg: (value) => {
+        if (value.length === 0) {
+          return "Task name is required";
+        } else if (value.length > 40) {
+          return "Task name too long";
+        } else {
+          return "";
+        }
+      },
+    },
+    taskPriority: {
+      getErrorMsg: () => "",
+      // getErrorMsg: (value) => (value !== "0" ? "" : "Task priority must be low"), // example
+    },
+  };
+
   const {
     values: { taskName, taskPriority },
     errors,
     dirty,
+    isReady,
     handleOnChange,
     handleOnSubmit,
-    disable,
-  } = useForm(stateSchema, stateValidatorSchema, onSubmitForm);
+  } = useForm(formStateSchema, onSubmitForm);
 
   return (
     <div className="form-wrapper">
@@ -44,11 +44,10 @@ const Form = ({ setTodos }) => {
             type="text"
             name="taskName"
             placeholder="Enter new task"
-            required
             value={taskName}
             onChange={handleOnChange}
           />
-          {errors.taskName && dirty.taskName && (
+          {errors.taskName && dirty && (
             <p className="error">{errors.taskName}</p>
           )}
         </div>
@@ -65,11 +64,15 @@ const Form = ({ setTodos }) => {
             <option value="1">Medium</option>
             <option value="2">High</option>
           </select>
-          {errors.taskPriority && dirty.taskPriority && (
+          {errors.taskPriority && dirty && (
             <p className="error">{errors.taskPriority}</p>
           )}
         </div>
-        <button form="add-task" type="submit" disabled={disable}>
+        <button
+          form="add-task"
+          type="submit"
+          className={isReady ? "active" : "inactive"}
+        >
           <FontAwesomeIcon icon={faPlusSquare} />
         </button>
       </form>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,153 +1,69 @@
-import { useState, useEffect, useCallback } from "react";
+import { useEffect, useState } from "react";
 
-const VALUE = "value";
-const ERROR = "error";
-const REQUIRED_FIELD_ERROR = "This field is required";
-
-function is_object(value) {
-  return typeof value === "object" && value !== null;
+function createInitialObject(schema, val) {
+  return Object.keys(schema).reduce((acc, cur) => ({ ...acc, [cur]: val }), {});
 }
 
-function is_required(value, isRequired) {
-  if (!value && isRequired) return REQUIRED_FIELD_ERROR;
-  return "";
-}
+function useForm(stateSchema, submitFormCallback) {
+  const [values, setValues] = useState(createInitialObject(stateSchema, ""));
+  const [errors, setErrors] = useState(createInitialObject(stateSchema, ""));
+  const [dirty, setDirty] = useState(false);
+  const [isReady, setIsReady] = useState(false);
 
-function get_prop_values(stateSchema, prop) {
-  return Object.keys(stateSchema).reduce((accumulator, curr) => {
-    accumulator[curr] = !prop ? false : stateSchema[curr][prop];
-
-    return accumulator;
-  }, {});
-}
-
-function useForm(
-  stateSchema = {},
-  stateValidatorSchema = {},
-  submitFormCallback
-) {
-  const [state, setStateSchema] = useState(stateSchema);
-
-  const [values, setValues] = useState(get_prop_values(state, VALUE));
-  const [errors, setErrors] = useState(get_prop_values(state, ERROR));
-  const [dirty, setDirty] = useState(get_prop_values(state));
-
-  const [disable, setDisable] = useState(true);
-  const [isDirty, setIsDirty] = useState(false);
-
-  // Get a local copy of stateSchema
   useEffect(() => {
-    setStateSchema(stateSchema);
-    setDisable(true); // Disable button in initial render.
-    setInitialErrorState();
-  }, []); // eslint-disable-line
-
-  // For every changed in our state this will be fired
-  // To be able to disable the button
-  useEffect(() => {
-    if (isDirty) {
-      setDisable(validateErrorState());
-    }
-  }, [errors, isDirty]); // eslint-disable-line
-
-  // Validate fields in forms
-  const validateFormFields = useCallback(
-    (name, value) => {
-      const validator = stateValidatorSchema;
-      // Making sure that stateValidatorSchema name is same in stateSchema
-      if (!validator[name]) return;
-
-      const field = validator[name];
-
-      let error = "";
-      error = is_required(value, field.required);
-
-      if (is_object(field["validator"]) && error === "") {
-        const fieldValidator = field["validator"];
-
-        // Test the function callback if the value meets the criteria
-        const testFunc = fieldValidator["func"];
-        if (!testFunc(value, values)) {
-          error = fieldValidator["error"];
+    setIsReady(() => {
+      for (const key in stateSchema) {
+        if (stateSchema[key].getErrorMsg(values[key])) {
+          return false;
         }
       }
+      return true;
+    });
+  }, [values, stateSchema, errors]);
 
-      return error;
-    },
-    [stateValidatorSchema, values]
-  );
+  const handleOnChange = (event) => {
+    setDirty(true);
 
-  // Set Initial Error State
-  // On first render...
-  const setInitialErrorState = useCallback(() => {
-    Object.keys(errors).map((name) =>
-      setErrors((prevState) => ({
-        ...prevState,
-        [name]: validateFormFields(name, values[name]),
-      }))
-    );
-  }, [errors, values, validateFormFields]);
+    const { name, value } = event.target;
+    const { getErrorMsg } = stateSchema[name];
+    const errorMsg = getErrorMsg(value);
 
-  // Used to disable submit button if there's a value in errors
-  // or the required field in state has no value.
-  // Wrapped in useCallback to cached the function to avoid intensive memory leaked
-  // in every re-render in component
-  const validateErrorState = useCallback(
-    () => Object.values(errors).some((error) => error),
-    [errors]
-  );
+    setErrors((prevState) => ({
+      ...prevState,
+      [name]: errorMsg || "",
+    }));
 
-  // Event handler for handling changes in input.
-  const handleOnChange = useCallback(
-    (event) => {
-      setIsDirty(true);
+    setValues((prevState) => ({ ...prevState, [name]: value }));
+  };
 
-      const name = event.target.name;
-      const value = event.target.value;
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    setDirty(true);
 
-      const error = validateFormFields(name, value);
-
-      setValues((prevState) => ({ ...prevState, [name]: value }));
-      setErrors((prevState) => ({ ...prevState, [name]: error }));
-      setDirty((prevState) => ({ ...prevState, [name]: true }));
-    },
-    [validateFormFields]
-  );
-
-  const handleOnSubmit = useCallback(
-    (event) => {
-      event.preventDefault();
-
-      // Making sure that there's no error in the state
-      // before calling the submit callback function
-      if (!validateErrorState()) {
-        submitFormCallback(values);
-        setValues(get_prop_values(state, VALUE));
-        setErrors(get_prop_values(state, ERROR));
-        setDirty(get_prop_values(state));
-        setIsDirty(false);
-        setDisable(true);
-        setInitialErrorState();
-      }
-    },
-    [
-      validateErrorState,
-      submitFormCallback,
-      values,
-      state,
-      setInitialErrorState,
-    ]
-  );
+    if (isReady) {
+      submitFormCallback(values);
+      setValues(createInitialObject(stateSchema, ""));
+      setErrors(createInitialObject(stateSchema, ""));
+      setDirty(false);
+      setIsReady(false);
+    } else {
+      setErrors((prevErrors) => {
+        const newErrors = { ...prevErrors };
+        for (const key in newErrors) {
+          newErrors[key] = stateSchema[key].getErrorMsg(values[key]);
+        }
+        return newErrors;
+      });
+    }
+  };
 
   return {
-    handleOnChange,
-    handleOnSubmit,
     values,
     errors,
-    disable,
-    setValues,
-    setErrors,
     dirty,
+    isReady,
+    handleOnChange,
+    handleOnSubmit,
   };
 }
 
