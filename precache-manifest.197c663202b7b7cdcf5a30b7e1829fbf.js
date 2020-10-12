698625e260cd35fababe1abc4b27963c6d71e2c5
Łukasz Walaszczyk <l.walaszczyk@interia.pl>
2020-10-12 14:02:48 +0200

    Updates

---
+++ precache-manifest.197c663202b7b7cdcf5a30b7e1829fbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29cf8d2a42f95ddd77f9b485e35bfca8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "d7ba950e4bbe88bd7764",
+    "url": "/todo/static/css/main.c3e99cef.chunk.css"
+  },
+  {
+    "revision": "acc7ffa40e211b63bcdf",
+    "url": "/todo/static/js/2.2b81a9cd.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/todo/static/js/2.2b81a9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ba950e4bbe88bd7764",
+    "url": "/todo/static/js/main.fe77fc83.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
